2905059e385eaabed8383ac9b2db7b808ea92d20
Matt Tobin <matt@matttobin.co.uk>
2017-09-16 21:34:18 +0100

    Add localstorage functionality

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 const app = new Vue({
   el: '#app',
   data: {
-    todos: [
-      {
-        name: 'A to do',
-        complete: false
-      },
-      {
-        name: 'Another to do',
-        complete: false
-      },
-      {
-        name: 'And another one',
-        complete: false
-      }
-    ],
+    // Get todos from localstorage or create empty array
+    todos: JSON.parse(localStorage.getItem('vue-todos')) || [],
     newTodo: {
       name: ''
     }
   },
   methods: {
     addTodo() {
-      this.todos.push({
-        name: this.newTodo.name,
-        complete: false
-      });
-      this.newTodo.name = '';
+      // Only add a todo if a name exists
+      if(this.newTodo.name != '') {
+        this.todos.push({
+          name: this.newTodo.name,
+          complete: false
+        });       
+        
+        this.newTodo.name = '';
+      }
     },
     removeTodo(todo) {
       this.todos.splice(this.todos.indexOf(todo), 1)
+    }
+  },
+  watch: {
+    // Watch for changes in todos
+    todos: {
+      handler: function(todos) {
+        // Save todos to localstorage
+        window.localStorage.setItem('vue-todos', JSON.stringify(todos));
+      },
+      // Watch for changes in nested properties
+      deep: true
     }
   }
 });
