2032d34137d8543b67f5b16aec6316a39a30e566
Matt Tobin <matt@matttobin.co.uk>
2017-09-17 16:55:20 +0100

    Separate add task form to its own component

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
-const app = new Vue({
-  el: '#app',
-  data: {
-    // Get todos from localstorage or create empty array
-    todos: JSON.parse(localStorage.getItem('vue-todos')) || [],
-    // Temporarily store new todo info
-    newTodo: {
-      name: ''
+// Add Task Form component
+Vue.component('add-task-form', {
+  template: `
+    <div>
+      <form v-on:submit.prevent="addTodo">
+        <h3>Add a new task</h3>
+        <input type="text" v-model="newTodo">
+        <input type="submit" value="Add">
+      </form>
+    </div>
+  `,
+  data() {
+    return {
+      newTodo: ''
     }
   },
   methods: {
     addTodo() {
       // Only add a todo if a name exists
-      if(this.newTodo.name != '') {
-        this.todos.push({
-          name: this.newTodo.name,
+      if(this.newTodo != '') {
+        const todo = {
+          name: this.newTodo,
           complete: false
-        });
-        
+        };
+        // Emit add-todo event to parent with new todo
+        this.$emit('add-todo', todo);
         // Reset new todo name
-        this.newTodo.name = '';
+        this.newTodo = '';
       }
+    }
+  }
+});
+
+// Main Vue instance
+const app = new Vue({
+  el: '#app',
+  data: {
+    // Get todos from localstorage or create empty array
+    todos: JSON.parse(localStorage.getItem('vue-todos')) || []
+  },
+  methods: {
+    addNewTodo(todo) {
+      // Add new todo to todos
+      this.todos.push(todo);
     },
     removeTodo(todo) {
+      // Remove todo from todos
       this.todos.splice(this.todos.indexOf(todo), 1)
     },
     updateTodo(todo) {
